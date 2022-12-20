46e52a0723ede795a47dccc70c7672a44004f636
unknown <therealcyberpunk88@gmx.de>
2022-12-20 10:18:52 +0100

    first commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1620461.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 115773937, 115773945, 115773947, 115773961, 115773964, 115773971 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/115773930/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/115773931/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/115773929/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/115773934/1/basis.wasm.js', 'wasmUrl' : 'files/assets/115773935/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/115773933/1/basis.js', 'preload' : false},
+];
